a8bf823685e751936cfbd938f1d5a0bc9f85e124
Timotheo <ccs.timotheoche@gmail.com>
2025-02-01 01:26:36 +0800

    Add htmx with tailwind for index page template

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/timocheu/shortify/utils"
 )
@@ -20,13 +21,16 @@ func main() {
 
 	http.HandleFunc("/", indexPage)
 	http.HandleFunc("/shorten", shortenPage)
+	http.HandleFunc("/r/{code}", redirect)
 
 	// Listener for incoming request
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func indexPage(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("template/index.html"))
 
+	tmpl.Execute(w, nil)
 }
 
 func shortenPage(w http.ResponseWriter, r *http.Request) {
@@ -34,10 +38,33 @@ func shortenPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Payload: ", url)
 
 	shortURL := utils.GetShortCode()
-	// fullShortURL := fmt.Sprintf("http://localhost:8080/r/%s", shortURL)
+	fullShortURL := fmt.Sprintf("http://localhost:8080/r/%s", shortURL)
 
 	// Print generated url
 	fmt.Println("Generated URL: ", shortURL)
 
 	utils.SetKey(&ctx, dbClient, shortURL, url, 0)
+
+	// Write the response into the responese write
+	fmt.Fprintf(w, `<p class="mt-4 text-blue-400">Shortened URL: <a href="/r/%s" class="underline">%s</a></p>`, shortURL, fullShortURL)
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	key := r.PathValue("code")
+
+	// Check if the key exist or empty
+	if key == "" {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
+	}
+
+	longURL, err := utils.GetLongURL(&ctx, dbClient, key)
+	// Check if URL exist in redis server
+	if err != nil {
+		http.Error(w, "Unable to find the Shortened URL", http.StatusBadRequest)
+		return
+	}
+
+	// Do the redirect after all error check
+	http.Redirect(w, r, longURL, http.StatusPermanentRedirect)
 }
